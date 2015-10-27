e6f1e126904057fc37d2e4211f33a45d9c858302
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-10-27 18:27:26 +0100

    Make sure that we initialize the FS if the volume is new

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -17,6 +17,10 @@ instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 aws ec2 attach-volume --volume-id $BLOCK_STORAGE_ID --instance-id $instance_id --device /dev/xvdh
 sleep 5
 mkdir -p /mnt/jenkins_data
+blkid -o list | grep xvdh
+if [ $? -eq 1 ]; then
+    mkfs -t ext4 /dev/xvdh
+fi
 mount /dev/xvdh /mnt/jenkins_data
 aws s3 cp s3://$BUCKET/hub.dockerconfig .dockerconfig
 docker pull kiddouk/jenkins
