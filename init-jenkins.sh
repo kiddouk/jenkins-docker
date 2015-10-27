9614b4e30d4a408f693a5eb40675b315812ccc1f
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-10-27 16:51:12 +0100

    Update init script

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -13,8 +13,9 @@ aws configure set output json
 apt-get install curl
 instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 aws ec2 attach-volume --volume-id $BLOCK_STORAGE_ID --instance-id $instance_id --device /dev/xvdh
+sleep 5
 mkdir -p /mnt/jenkins_data
 mount /dev/xvdh /mnt/jenkins_data
-aws s3 cp s3://$BUCKET/.dockerconfig .
+aws s3 cp s3://$BUCKET/hub.dockerconfig .dockerconfig
 docker pull kiddouk/jenkins
 docker run -p 8080:8080 -v /mnt/jenkins_data:/var/jenkins_home kiddouk/jenkins
