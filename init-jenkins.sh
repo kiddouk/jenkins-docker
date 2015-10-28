50ac7e80a7274c9143159bf94c7731742cb1da70
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-10-28 12:09:46 +0100

    Allow reentrant

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -7,7 +7,7 @@ BLOCK_STORAGE_ID=$2
 BUCKET=$3
 
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-engine curl python-setuptools 
 easy_install pip
