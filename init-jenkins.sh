d000990244d3f0993605b297350f3f394e7238d5
Sébastien Requiem <sebastien.requiem@gmail.com>
2015-10-28 11:52:47 +0100

    Install proper jenkins repo

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -6,8 +6,10 @@ AWS_REGION=$1
 BLOCK_STORAGE_ID=$2
 BUCKET=$3
 
-
-apt-get install -y docker.io curl python-setuptools 
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engin curl python-setuptools 
 easy_install pip
 pip install awscli
 pip install requests
